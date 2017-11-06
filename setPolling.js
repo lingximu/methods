b0203f6b90c5489f936f3e286bcb97f1a0c0478e
lingximu <lingxiweb@gmail.com>
2017-11-06 16:06:11 +0800

    requier bluebird

--- setPolling.js
+++ setPolling.js
@@ -11,7 +11,7 @@
  * @property {false | function} destroyCondition - 清除轮询的条件,为false则不主动进行清除，,default false 需调用者自行调用返回的pollingControl.destroy进行清除；
  *                                                 接收的参数 result,count ,result代表fn返回的结果，count代表fn【已经执行的次数】
  * */
-// var Promise = require("bluebird");
+var Promise = require("bluebird");
 
 function setPolling(options) {
     // params init
