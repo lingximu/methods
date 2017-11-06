11742caf515f057bdeb29cad21af40f4d7094164
lingximu <lingxiweb@gmail.com>
2017-11-06 15:20:16 +0800

    添加完整测试用例

--- setPolling.js
+++ setPolling.js
@@ -1,16 +1,17 @@
 /**
  *
  * @param {object} options
- * @property {!function} fn - 轮询的函数
+ * @property {!function} fn - 轮询的函数; 接收的参数 count,res 第一个参数count为fn【已经执行的次数】，第二个参数res为execCondition返回的结果
  * @property {!number} interval - 轮询的时间间隔
  * @property {false | function} handleCatch - fn运行异常时的处理函数，接受error。default false
- * @property {function} handleThen - fn运行返回数据的处理函数，接受result。default emptyFunction
+ * @property {function} handleThen - fn运行返回数据的处理函数，default emptyFunction; 接收的参数，result为fn返回的结果
  * @property {boolean} delay - 第一次是否延迟执行,default false
  * @property {boolean} debug - 是否打开调试，当为true时，会将执行次数在控制台打印出来，defaut false
- * @property {false | function} execCondition - 执行fn的条件,为false则每次都执行，default false
- * @property {false | function} destroyCondition - 清除轮询的条件,为false则不主动进行清除，需调用者自行调用返回的pollingControl.destroy进行清除，接受result和count,default false
- */
-var Promise = require("bluebird");
+ * @property {false | function} execCondition - 执行fn的条件,为false则每次都执行，default false; 接收的参数 count, 代表fn已经执行的次数
+ * @property {false | function} destroyCondition - 清除轮询的条件,为false则不主动进行清除，,default false 需调用者自行调用返回的pollingControl.destroy进行清除；
+ *                                                 接收的参数 result,count ,result代表fn返回的结果，count代表fn【已经执行的次数】
+ * */
+// var Promise = require("bluebird");
 
 function setPolling(options) {
     // params init
@@ -43,36 +44,40 @@ function setPolling(options) {
 
     // variable init
     let timeoutId = null;
-    let count = 0;
 
+    let count = 0;
     // main function init
     const doStuff = function doStuff() {
-        if (!execCondition || execCondition(count)) {
+        let res;
+
+        if (execCondition === false || (res = execCondition(count))) {
             new Promise(function (resolve, reject) {
-                count++; // 计算轮询次数
                 debug && console.debug(`${fn.toString().slice(0, 20)}将调用第${count}次`)
-                resolve(fn())
+                resolve(fn(count, res))
             }).then(result => {
+                count++;
+                handleThen(result)
                 if (destroyCondition && destroyCondition(result, count))
                     destroy()
-
-                handleThen(result)
                 // can't execute fn in next time if destroy has call
                 if(timeoutId !== null )
                     timeoutId = setTimeout(doStuff, interval)
             }).catch(e => {
-                if (!handleCatch) {
+                if (handleCatch === false) {
                     destroy()
                     debug && console.error('please set handleCatch in setPolling', e)
                 } else {
                     handleCatch(e, count)
                 }
             })
+        }else{
+            timeoutId = setTimeout(doStuff, interval)
         }
     }
 
     // destroy setTimeout
     const destroy = function () {
+        debugger;
         clearTimeout(timeoutId)
         timeoutId = null;
     }
@@ -93,7 +98,7 @@ function setPolling(options) {
             return false;
         },
         count: function () {
-            return count; // return execute count
+            return count; // 返回fn【已经执行】的次数
         }
     };
 }
--- test/setPolling.test.js
+++ test/setPolling.test.js
@@ -1,65 +1,113 @@
-require('babel-polyfill')
 const { setPolling } = require('../index.js')
 const { expect, assert } = require('chai')
+const Promise = require('bluebird')
 const axios = require('axios')
 const sinon = require('sinon')
 
-describe('setPolling', function(){
-    var sandbox = sinon.createSandbox();
-
+describe('setPolling测试', function(){
+    let sandbox = sinon.createSandbox({});
     beforeEach(function() {
         // sandbox.spy(axios, 'get')
         sandbox.stub(axios, 'get').callsFake(function fakeFn() {
-            return '{"one":1}';
+            return Promise.resolve('resolves sinon promise data');
         });
     })
 
     afterEach(function(){
+
         sandbox.restore()
     })
 
-    it('xhr count == callback count == pollingControl.count()', function(done){
+    it('xhr请求次数、回调次数、pollingControl返回的次数应该相同', function(done){
         var callback = sandbox.spy()
 
         const pollingControl = setPolling({
             fn: () => axios.get('/__zuul'),
             interval: 100,
-            debug: true,
             handleThen: callback
         })
 
         setTimeout(function () {
             pollingControl.destroy();
-            sandbox.assert.callCount(axios.get, pollingControl.count())
-            sandbox.assert.callCount(callback, pollingControl.count())
-            assert(pollingControl.isDestroy(), 'pollingControl should destroy')
+
+            sandbox.assert.callCount(axios.get, 5)
+            sandbox.assert.callCount(callback, 5)
+            assert(pollingControl.count() === 5, 'pollingControl.count should 5')
             done();
 
-        }, 1200);
+        }, 450);
     })
 
 
-    it('pollingControl.destroy is valid', function (done) {
+    it('pollingControl.destroy生效', function (done) {
         var callback = sandbox.spy()
 
         const pollingControl = setPolling({
             fn: () => axios.get('/__zuul'),
             interval: 100,
-            debug: true,
             handleThen: callback
         })
         setTimeout(function() {
             pollingControl.destroy();
-        }, 1000);
+        }, 100);
 
         setTimeout(function () {
             assert(pollingControl.isDestroy(), 'pollingControl should destroy')
             done();
-        }, 1200);
+        }, 120);
+    })
+
+    it('execCondition 生效', function (done) {
+        var callback = sandbox.spy()
+        let count = 0;
+        const pollingControl = setPolling({
+            fn: () => axios.get('/__zuul'),
+            interval: 100,
+            handleThen: callback,
+            execCondition: () => count++ % 2
+        })
+        setTimeout(function() {
+            pollingControl.destroy();
+            sandbox.assert.callCount(axios.get, 3)
+            sandbox.assert.callCount(callback, 3)
+            done();
+        }, 550);
+    })
+
+    it('destroyCondition 生效', function (done) {
+        var callback = sandbox.spy()
+        const pollingControl = setPolling({
+            fn: () => axios.get('/__zuul'),
+            interval: 100,
+            handleThen: callback,
+            destroyCondition: (result, count) => count >= 5
+        })
+        setTimeout(function() {
+            pollingControl.destroy();
+            sandbox.assert.callCount(axios.get, 5)
+            sandbox.assert.callCount(callback, 5)
+            assert(pollingControl.count() === 5, 'pollingControl.count should 5')
+            done();
+        }, 800);
     })
 
 
-
+    it('handleThen接收的参数与fn返回的参数一致', function (done) {
+        let along = 'aaaaaaaaaaaaaaaaaaaaaaa'
+        let blong;
+        const pollingControl = setPolling({
+            fn: () => along,
+            interval: 100,
+            handleThen: (result) => blong = result
+        })
+        setTimeout(function () {
+            pollingControl.destroy();
+        }, 100);
+        setTimeout(function () {
+            assert(along === blong, 'along should equal blong')
+            done();
+        }, 120);
+    })
 
 })
 
